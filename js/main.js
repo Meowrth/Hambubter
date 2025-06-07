55d184ca60cd6a1480cc34e8a8e4bedfb771c7a6
hbubcoin <djobss@naver.com>
2025-06-07 14:40:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   if (window.location.hash) {
     history.replaceState(null, '', window.location.href.split('#')[0]);
-});
+}
+  
 document.getElementById("menu-toggle").addEventListener("click", function () {
   const menu = document.getElementById("menu-links");
   menu.classList.toggle("menu-open");
